24b42e5c177c2cac949040c2fe665ff4ed8f0b8c
Skarakhod.OE <skorokhod.oe@nsd.ru>
2020-02-24 19:56:46 +0300

    add spring-inmemory.xml, table meals, change ControllerTest, SpringMain

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -9,3 +9,14 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, description, calories) VALUES
+(100000, '2020-02-22 08:00','breakfast', 1000),
+(100000, '2020-02-22 10:30','brunch', 600),
+(100000, '2020-02-23 13:00','lunch', 700),
+(100000, '2020-02-23 20:20','dinner', 350),
+(100000, '2020-02-24 23:10','evening meal', 200),
+(100001, '2020-02-22 09:10','breakfast', 1000),
+(100001, '2020-02-23 11:30','brunch', 800),
+(100001, '2020-02-24 13:20','lunch', 500),
+(100001, '2020-02-24 23:30','evening meal', 100);
